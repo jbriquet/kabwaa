d77794576446669ff81de98e7320e22169728ed3
jp <jeanpierre.briquet@gmail.com>
2016-08-23 23:07:16 +0200

    create function “home” to load html home page when function “home” is called

---
+++ kabwaa_django/kabwaa_django/views.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def home(request):
+    return render_to_response("index.html", RequestContext(request, {}))
